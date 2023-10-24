b502eb5da06a2700eacffa427d8b9a18b110f9cd
Jean Luis Gonzalez <jeanluiss26@gmail.com>
2023-10-24 16:55:37 -0400

    update at work

--- app/api/stores/route.ts
+++ app/api/stores/route.ts
@@ -9,6 +9,7 @@ export async function POST(req: Request) {
     const body = await req.json();
 
     const { name } = body;
+
     if (!userId) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -18,6 +18,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 const formSchema = z.object({
   name: z.string().min(1),
@@ -41,9 +42,9 @@ export const StoreModal = () => {
 
       const response = await axios.post("/api/stores", values);
 
-      console.log(response.data);
+      toast.success("Store created.");
     } catch (error) {
-      console.log(error);
+      toast.error("Something went wrong.");
     } finally {
       setLoading(false);
     }
