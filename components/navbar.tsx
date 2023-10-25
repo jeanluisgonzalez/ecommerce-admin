038bf53c17eda98f9b4a3881e9136b0f2a7e082d
Jean Luis Gonzalez <jeanluiss26@gmail.com>
2023-10-25 16:58:22 -0400

    update at work

---
+++ components/navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <div className="border-b">
+      <div className="flex h-16 items-center px-4">
+        <div>this will be a store switcher</div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
