315000e474b85277a6a2153cf78f30f0848c6022
Jean Luis Gonzalez <jeanluiss26@gmail.com>
2023-11-20 16:32:46 -0400

    update at work

--- app/(dashboard)/[storeId]/(routes)/sizes/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/sizes/components/cell-action.tsx
@@ -15,11 +15,11 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
 
-import { BillboardColumn } from "./columns";
+import { SizeColumn } from "./columns";
 import { AlertModal } from "@/components/modals/alert-modal";
 
 interface CellActionProps {
-  data: BillboardColumn;
+  data: SizeColumn;
 }
 
 export const CellAction: React.FC<CellActionProps> = ({ data }) => {
@@ -31,19 +31,17 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
 
   const onCopy = (id: string) => {
     navigator.clipboard.writeText(id);
-    toast.success("Billboard id copied to the clipboard.");
+    toast.success("Size id copied to the clipboard.");
   };
 
   const onDelete = async () => {
     try {
       setLoading(true);
-      await axios.delete(`/api/${params.storeId}/billboards/${data.id}`);
+      await axios.delete(`/api/${params.storeId}/sizes/${data.id}`);
       router.refresh();
-      toast.success("Billboard deleted.");
+      toast.success("Size deleted.");
     } catch (error) {
-      toast.error(
-        "Make sure you removed all categories using this billboard first."
-      );
+      toast.error("Make sure you removed all products using this size first.");
     } finally {
       setLoading(false);
       setOpen(false);
@@ -72,9 +70,7 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
             Copy Id
           </DropdownMenuItem>
           <DropdownMenuItem
-            onClick={() =>
-              router.push(`/${params.storeId}/billboards/${data.id}`)
-            }
+            onClick={() => router.push(`/${params.storeId}/sizes/${data.id}`)}
           >
             <Edit className="mr-2 h-4 w-4" />
             Update
--- app/(dashboard)/[storeId]/(routes)/sizes/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/sizes/page.tsx
@@ -1,11 +1,11 @@
 import prismadb from "@/lib/prismadb";
 import { format } from "date-fns";
 
-import { BillboardClient } from "./components/client";
-import { BillboardColumn } from "./components/columns";
+import { SizesClient } from "./components/client";
+import { SizeColumn } from "./components/columns";
 
 const SizesPage = async ({ params }: { params: { storeId: string } }) => {
-  const billboards = await prismadb.sizes.findMany({
+  const sizes = await prismadb.size.findMany({
     where: {
       storeId: params.storeId,
     },
@@ -14,16 +14,17 @@ const SizesPage = async ({ params }: { params: { storeId: string } }) => {
     },
   });
 
-  const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
+  const formattedSizes: SizeColumn[] = sizes.map((item) => ({
     id: item.id,
-    label: item.label,
+    name: item.name,
+    value: item.value,
     createdAt: format(item.createdAt, "MMM do, yyyy"),
   }));
 
   return (
     <div className="flex-col">
       <div className=" flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient data={formattedBillboards} />
+        <SizesClient data={formattedSizes} />
       </div>
     </div>
   );
